15f2ccf38683308c1809166e1a33901e452e3e66
ZhangBing <v_zhangbing@baidu.com>
2017-07-02 13:18:54 +0800

    修改

--- src/main/java/com/example/demo/requestlimit/annotation/EnableRequestLimit.java
+++ src/main/java/com/example/demo/requestlimit/annotation/EnableRequestLimit.java
@@ -1,7 +1,6 @@
 package com.example.demo.requestlimit.annotation;
 
 import com.example.demo.requestlimit.RequestLimitConfiguration;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
@@ -12,13 +11,11 @@ import java.lang.annotation.RetentionPolicy;
 /**
  * Created by v_zhangbing on 2017/7/2.
  * 总开关, 启用访问限制功能.
- * 这个注解需要再SpringBoot主类上添加.
- * 这个注解表明自己是一个配置类 @Configuration  同时引入了真正的配置类  @Import(RequestLimitConfiguration.class)
+ * 这个注解需要再SpringBoot主类上添加. 它引入了真正的配置类  @Import(RequestLimitConfiguration.class)
  */
 @Retention(RetentionPolicy.RUNTIME)
 @EnableAspectJAutoProxy
 @Documented
-@Configuration
 @Import(RequestLimitConfiguration.class)
 public @interface EnableRequestLimit {
 }
